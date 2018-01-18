18cdec9784c2f9eb459f9d23007bd349ac9c2d03
Tristan Benedict-Hall <tbenedicthall@Tristans-MBP.fios-router.home>
2018-01-18 16:19:23 -0500

    updated portfolio

---
+++ assets/js/tbh.js
@@ -0,0 +1,113 @@
+    $(document).ready(function() {
+
+    	// // var work1Div = $("<div class='row col-6'>");
+    	// // work1Div.text("Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo.");
+
+    	// $("#work-1").on("click", function toggleDiv(work1Div) {
+
+     //        $("#work1Div").slideToggle();
+     //        });
+
+    	// 	if($("#work2Div").is(':visible')){
+     //        	$("#work2Div").toggle();
+     //        };
+    	// 	if($("#work3Div").is(':visible')){
+     //        	$("#work3Div").toggle();
+     //        };
+    	// 	if($("#work4Div").is(':visible')){
+     //        	$("#work4Div").toggle();
+     //        };			
+     //        if($("#work5Div").is(':visible')){
+     //        	$("#work5Div").toggle();
+     //        };	
+            
+    	// $("#work-2").on("click", function toggleDiv(work1Div) {
+
+     //        $("#work2Div").slideToggle();
+     //        });
+
+    	// 	if($("#work1Div").is(':visible')){
+     //        	$("#work1Div").toggle();
+     //        };
+     //        if($("#work3Div").is(':visible')){
+     //        	$("#work3Div").toggle();
+     //        };
+    	// 	if($("#work4Div").is(':visible')){
+     //        	$("#work4Div").toggle();
+     //        };	
+     //        if($("#work5Div").is(':visible')){
+     //        	$("#work5Div").toggle();
+     //        };	
+
+    	// $("#work-3").on("click", function toggleDiv(work1Div) {
+
+     //        $("#work3Div").slideToggle();
+     //        });
+
+     //        if($("#work1Div").is(':visible')){
+     //        	$("#work1Div").toggle();
+     //        };
+     //        if($("#work2Div").is(':visible')){
+     //        	$("#work2Div").toggle();
+     //        };
+    	// 	if($("#work4Div").is(':visible')){
+     //        	$("#work4Div").toggle();
+     //        };	
+     //        if($("#work5Div").is(':visible')){
+     //        	$("#work5Div").toggle();
+     //        };	
+
+    	// $("#work-4").on("click", function toggleDiv(work1Div) {
+
+     //        $("#work4Div").slideToggle();
+     //        });
+
+     //        if($("#work1Div").is(':visible')){
+     //        	$("#work1Div").toggle();
+     //        };
+     //        if($("#work2Div").is(':visible')){
+     //        	$("#work2Div").toggle();
+     //        };
+    	// 	if($("#work3Div").is(':visible')){
+     //        	$("#work3Div").toggle();
+     //        };	
+     //        if($("#work5Div").is(':visible')){
+     //        	$("#work5Div").toggle();
+     //        };	
+
+    	// $("#work-5").on("click", function toggleDiv(work1Div) {
+
+     //        $("#work5Div").slideToggle();
+     //        });
+
+     //        if($("#work1Div").is(':visible')){
+     //        	$("#work1Div").toggle();
+     //        };
+     //        if($("#work2Div").is(':visible')){
+     //        	$("#work2Div").toggle();
+     //        };
+    	// 	if($("#work3Div").is(':visible')){
+     //        	$("#work3Div").toggle();
+     //        };	
+    	// 	if($("#work4Div").is(':visible')){
+     //        	$("#work4Div").toggle();
+     //        };
+
+
+
+            				
+      //   $("#work-row-1").append(work1Div);
+
+
+		// $("#work-1").on("click", function showDiv(work1Div) {
+		//      $('#work1Div').each(function(index) {
+		//           if ($(this).attr("id") == work1Div) {
+		//                $(this).show(200);
+		//           }
+		//           else {
+		//                $(this).hide(600);
+		//           }
+		//      });
+		// })
+
+      		});
